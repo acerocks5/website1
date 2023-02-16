ee2ebd5cf7013a9fcc001735e083f36d7bb571e5
Daniel Wegrzyn <danny.wegrzyn@hotmail.com>
2023-02-15 23:05:29 -0500

    added header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import './header.css';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt3__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>Let's Build Something amazing with GPT-3 OpenAI</h1>
+        <p>Random text stuff cool wow. This does stuff but not really it just made to look cool. TM</p>
+
+        <div className='gpt3__header-content__input'>
+          <input type="email" placeholder='Your Email Address'/>
+          <button type='button'>Get Started</button>
+        </div>
+
+        <div className='gpt3__header-content__people'>
+          <img src={people} alt="people" />
+          <p>1,000,000 people requested access in the last 24 hours *trust*</p>
+        </div>
+      </div>
+      <div className='gpt3__header-image'>
+        <img src={ai} alt="ai" />
+      </div>
+    </div>
   )
 }
 
